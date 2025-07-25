6b7f177fa7dbcfd20d92b571134e50a7e091af8d
albaykarayel <mizgi609@gmail.com>
2025-07-26 02:48:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const button = document.getElementById("moveButton");
+const trueButton = document.getElementById("trueButton");
+const content = document.getElementById("content");
+
+
+function getRandomPosition() {
+    const x = Math.random() * (window.innerWidth - button.offsetWidth - 10);
+    const y = Math.random() * (window.innerHeight - button.offsetHeight - 10);
+    return { 
+        x: Math.max(10, x), 
+        y: Math.max(10, y) 
+    };
+}
+
+button.addEventListener("mouseover", () => {
+    const { x, y } = getRandomPosition();
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+});
+
 trueButton.addEventListener("click", () => {
     content.innerHTML = `
         <img src="albaycat.jpg">
@@ -12,8 +32,9 @@ trueButton.addEventListener("click", () => {
     audio.autoplay = true;
 
     content.appendChild(audio);
+
+
     audio.play().catch(e => {
         console.log("Oynatma hatası:", e);
     });
 });
-
